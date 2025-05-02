d6d40bbc59889b9b98d4311aad66e2473d7ffbee
menasy <mehmetnasim42@gmail.com>
2025-05-02 17:09:52 +0300

    ip check

--- include/WebServer.hpp
+++ include/WebServer.hpp
@@ -6,7 +6,7 @@
 /*   By: menasy <menasy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 15:40:04 by menasy            #+#    #+#             */
-/*   Updated: 2025/05/02 15:50:22 by menasy           ###   ########.fr       */
+/*   Updated: 2025/05/02 16:25:18 by menasy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,12 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <cstring>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <iterator>
 # include "ServerConf.hpp"
 class WebServer {
 	private:
 		std::vector<ServerConf> serverConfVec;
+		std::map<int,ServerConf> socketMap;
 	public:
 		WebServer();
 		WebServer(std::vector<ServerConf>& serverConfVec);
@@ -36,6 +30,8 @@ class WebServer {
 		~WebServer();
 		
 		void initSocket();
+		bool isExistIpAndPort(const std::string& ip, int& port);
+		
 		
 
 };
--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -6,7 +6,7 @@
 /*   By: menasy <menasy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 15:50:42 by menasy            #+#    #+#             */
-/*   Updated: 2025/05/02 15:51:04 by menasy           ###   ########.fr       */
+/*   Updated: 2025/05/02 16:26:48 by menasy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,14 @@ WebServer &WebServer::operator=(const WebServer &other) {
 	return *this;
 }
 WebServer::~WebServer() {
+}
+
+bool WebServer::isExistIpAndPort(const std::string& ip, int& port)
+{
+	std::map<int,ServerConf>::iterator it;
+	it = this->socketMap.begin();
+	while(it++ != this->socketMap.end())
+		if (it->second.getIp() == ip && it->second.getPort() == port)
+			true;
+	return false;
 }
